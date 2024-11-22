d643d010dcb246d58ed76c1cf10dfa57e8701fbd
inyong <inyong2050@gmail.com>
2024-11-22 13:36:34 +0900

    port change

--- main.tf
+++ main.tf
@@ -30,7 +30,7 @@ resource "aws_security_group" "hashicat" {
 
   ingress {
     from_port   = 22
-    to_port     = 22
+    to_port     = 23
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
