bbe25cf69fc9817828812a3d6d2fe3a7a7fc2b0e
inyong <inyong2050@gmail.com>
2024-11-25 19:01:30 +0900

    blake-iam-policy

--- main.tf
+++ main.tf
@@ -25,10 +25,47 @@ provider "aws" {
   }
 }
 
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "role" {
+  name               = "test-role"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+data "aws_iam_policy_document" "policy" {
+  statement {
+    effect    = "Allow"
+    actions   = ["ec2:Describe*"]
+    resources = ["*"]
+  }
+}
+
 resource "aws_iam_policy" "policy" {
+  name        = "test-policy"
+  description = "A test policy"
+  policy      = data.aws_iam_policy_document.policy.json
+}
+
+resource "aws_iam_role_policy_attachment" "test-attach" {
+  role       = aws_iam_role.role.name
+  policy_arn = aws_iam_policy.policy.arn
+}
+
+
+resource "aws_iam_role_policy" "policy" {
   name        = "${var.prefix}-test-policy"
-  path        = "/"
-  description = "My test policy"
+  role = aws_iam_role.test_policy.id
 
   policy = jsonencode({
 	"Version": "2012-10-17",
@@ -49,6 +86,30 @@ resource "aws_iam_policy" "policy" {
 	  ]
   })
 }
+
+resource "aws_iam_role" "test_role" {
+  name = "SAML_Developer-1"
+  managed_policy_arns = "ReadOnlyAccess"
+  assume_role_policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Principal": {
+                "Federated": "arn:aws:iam::851725357209:saml-provider/blake-okta"
+            },
+            "Action": "sts:AssumeRoleWithSAML",
+            "Condition": {
+                "StringEquals": {
+                    "SAML:aud": "https://signin.aws.amazon.com/saml"
+                }
+            }
+        }
+    ]
+})
+}
+
+
 
 #data "aws_security_group" "example" {
 #  name                = aws_security_group.hashicat.name
