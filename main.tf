ecdca5027f7ce2b01e71e1bc0e4c598ba8c9fe45
inyong <inyong2050@gmail.com>
2024-11-28 17:52:17 +0900

    blake-iam-policy

--- main.tf
+++ main.tf
@@ -66,13 +66,15 @@ resource "aws_iam_policy" "cac-policy" {
 
 data "aws_iam_policy_document" "cac-policy" {
   statement {
+    sid = "1"
+
     actions = [
-      "s3:PutObject"
+      "s3:PutObject",
     ]
 
     resources = [
         "arn:aws:s3:::blake-test-1234567",
-        "arn:aws:s3:::blake-test-1234567/*"
+        "arn:aws:s3:::blake-test-1234567/*",
     ]
 
     condition {
